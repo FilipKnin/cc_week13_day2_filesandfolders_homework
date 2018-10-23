6d3946c3faf751df95ea899b063d6f8de0b62928
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-24 00:54:10 +0100

    Structure changed: components created

--- src/main/java/com/codeclan/homeworks/filesandfolders/repositories/FileRepository.java
+++ src/main/java/com/codeclan/homeworks/filesandfolders/repositories/FileRepository.java
@@ -3,5 +3,5 @@ package com.codeclan.homeworks.filesandfolders.repositories;
 import com.codeclan.homeworks.filesandfolders.models.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository  extends JpaRepository<File, Long> {
+public interface FileRepository extends JpaRepository<File, Long> {
 }
--- src/main/java/com/codeclan/homeworks/filesandfolders/repositories/UserRepository.java
+++ src/main/java/com/codeclan/homeworks/filesandfolders/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.codeclan.homeworks.filesandfolders.repositories;
 
-import com.codeclan.homeworks.filesandfolders.models.File;
+import com.codeclan.homeworks.filesandfolders.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<File, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
